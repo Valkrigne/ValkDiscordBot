59ac321c743dd46ba121d0688b21111f75912a29
= <atoons@gmail.com>
2016-07-24 13:07:34 +1000

    'a'

--- bin/install.sh
+++ bin/install.sh
@@ -14,7 +14,7 @@ else
   echo 'Edit config/application.yml to match your local setup'
 fi
 
-BUNDLE_GEMFILE=Gemfile bundle install
+BUNDLE_GEMFILE=gemfile bundle install
 rake db:create db:migrate
 
 # Import the SSH deployment key
