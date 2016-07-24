9d20837649ab55d5f6ebdd4a70e37d387906e8fc
= <atoons@gmail.com>
2016-07-24 12:19:52 +1000

    fix install script

--- bin/install.sh
+++ bin/install.sh
@@ -14,6 +14,9 @@ else
   echo 'Edit config/application.yml to match your local setup'
 fi
 
+BUNDLE_GEMFILE=gemfile bundle install
+rake db:create db:migrate
+
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
